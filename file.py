0c0732c670a609f28289c5a55f8f015d6c0279e7
Arthur Souza <amdsouza@inf.ufpel.edu.br>
2022-10-10 21:53:40 -0300

    first commit

---
+++ file.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 21:25:24 2022
+
+@author: arthur souza
+@author: rafael grimmler
+
+"""
+
+import pandas as pd
+
+endereço = 32
+byte = 8
+flag_saida = 0
+    
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
