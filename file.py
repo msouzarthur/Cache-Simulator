516b816152058522909dd160e69c0df6e801bfcc
Arthur Souza <amdsouza@inf.ufpel.edu.br>
2022-10-28 00:53:37 -0300

    funcionamento

--- file.py
+++ file.py
@@ -11,6 +11,7 @@ import os.path
 import math
 from pprint import pprint
 from pprint import pp
+from numpy import random
 
 assoc = 0
 bSize = 0
@@ -26,12 +27,13 @@ nMissConf = 0
 subs = 'R'
 memSize = 0
 mem = []
+fifoCount = 0
 
 def main():
     read() 
-    mem = build()
-    printCache(mem)
-    run(mem)
+    build()
+    run()
+    printCache()
 
 def read():
     #leitura de entrada
@@ -39,7 +41,7 @@ def read():
     
     #print("> cache_simulator <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada")
     cmd = input("> ")
-    cmd = 'cache_simulator 8 1 1 R 1 bin_100.bin'
+    cmd = 'cache_simulator 256 1 2 R 1 bin_10000.bin'
     if "cache_simulator" not in cmd:
         cmd += "c "
     cmd = cmd.split(" ")
@@ -70,21 +72,21 @@ def read():
 
 def build():
     #cria a memoria
-    global bOffset, nSets, bSize, assoc, memSize
-    
-    mem = []
+    global bOffset, nSets, bSize, assoc, memSize, mem, subs
     
     for i in range(assoc):
         sets = []
         for k in range(int(nSets)):
             val = 0
             tag = None
-            sets.append([val, tag])
+            pol = None
+            sets.append([val, tag, pol]) if subs=='L' else sets.append([val, tag])
         mem.append(sets)
-    return mem
     
     
-def printCache(mem):
+def printCache():
+    global nAcess, nHits, nMissComp, nMissCap, nMissConf
+    nMiss = nMissComp+nMissCap+nMissConf
     #exibe a memoria
     if flag == 0 :
         columns = ['validade','tag']
@@ -100,27 +102,18 @@ def printCache(mem):
             print("]")
         '''
     elif flag == 1:
-        print('normal')        
-        '''
-            • flag_saida = 1
-            o Formato padrão de saída que deverá respeitar a seguinte ordem: 
-            Total de acessos, Taxa de hit, Taxa de miss, Taxa de miss compulsório, 
-            Taxa de miss de capacidade, Taxa de miss de conflito 
-            o Ex: 100000, 0.95, 0.06, 0.01, 0.02, 0.03
-        '''        
+        print('{}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}'.format(nAcess, nHits/nAcess, nMiss/nAcess, nMissComp/(nMiss), nMissCap/(nMiss), nMissConf/(nMiss)))        
+     
     
-def run(mem):
+def run():
+    global mem, nAcess, nHits, nMissComp, nMissCap, nMissConf, fifoCount
+    
     f =  open(file,'rb')
     line = f.read(4)
     
-    nAcess=0
-    nHit = 0
-    
-    nCompul = 0
-    nCap = 0
-    nConf = 0
-    
+
     while line:
+        fHit = False
         nAcess += 1
         number = int.from_bytes(line, byteorder='big', signed=False)
 
@@ -134,36 +127,91 @@ def run(mem):
         #print("{} - {} - {} - {}".format(target,offset,index,tag))
         
         intOffset = int("".join(list(target[(32-offset):32])),2) if offset>0 else 0
-        intIndex = int("".join(list(target[(32-offset-index):32-offset])),2)
+        # ver erro
+        intIndex = int("".join(list(target[(32-offset-index):32-offset])),2) if index>0 else 0
+        
         intTag = int("".join(list(target[:(32-offset-index)])),2)
     
         cacheIndex = intIndex%nSets if intIndex > 0 else 0
         
         if assoc > 1:
-            for p in range(assoc):    
-                mem[p][cacheIndex]
+            #Xndex = isLineFull(cacheIndex)
+            #print(Xndex ,mem[0][cacheIndex][0],mem[1][cacheIndex][0],mem[2][cacheIndex][0],mem[3][cacheIndex][0])
+            for p in range(assoc):
+                if mem[p][cacheIndex][1] == intTag:
+                    nHits+=1
+                    fHit = True
+                    if subs=='L':
+                        for pp in range(assoc):
+                            if mem[pp][cacheIndex][2] != None:
+                                if mem[pp][cacheIndex][2] < mem[p][cacheIndex][2]:
+                                    mem[pp][cacheIndex][2]+=1
+                            mem[p][cacheIndex][2]=0
+            if not fHit:
+                setIndex = isLineFull(cacheIndex)
+                if setIndex == -1 :
+                    if isFull():
+                        nMissCap+=1
+                    else:
+                        nMissConf+=1
+                    if subs=='R':
+                        randomSet = random.randint(0, assoc-1)
+                        mem[randomSet][cacheIndex][1] = intTag
+                    elif subs=='F':
+                        
+                        mem[fifoCount][cacheIndex][1] = intTag
+                        if fifoCount < assoc - 1 :
+                            fifoCount+=1 
+                        else: 
+                            fifoCount = 0
+                    else: 
+                        pos = findBiggerPol(cacheIndex)
+                        for p in range(assoc):
+                            mem[p][cacheIndex][2]+=1
+                        mem[pos][cacheIndex][2] = 0
+                else:
+                    nMissComp+=1
+                    mem[setIndex][cacheIndex][0] = 1
+                    mem[setIndex][cacheIndex][1] = intTag             
+                    if subs=='L':
+                        mem[setIndex][cacheIndex][2] = 0
+                        for p in range(assoc):
+                            if setIndex != p and mem[p][cacheIndex][2]!=None:
+                                mem[p][cacheIndex][2]+=1             
         else:
             if mem[0][cacheIndex][0] == 0:
-                nCompul+=1
+                nMissComp+=1
                 mem[0][cacheIndex][0] = 1
                 mem[0][cacheIndex][1] = intTag
             elif mem[0][cacheIndex][1] == intTag:
-                nHit+=1
+                nHits+=1
             else:
-                if isFull(mem):
-                    nCap+=1
+                if isFull():
+                    nMissCap+=1
                 else:
-                    nConf+=1
-
-                mem[0][cacheIndex][1] = intTag
-            
-                
-        print('cache index - {}'.format(cacheIndex))
+                    nMissConf+=1
+                mem[0][cacheIndex][1] = intTag 
         line = f.read(4)
-           
-    print(mem[0])
+
+def findBiggerPol(line):
+    global mem, assoc
+    for p in range(assoc):
+        if mem[p][line][2] == assoc-1:
+            return p
+    return -1
+                    
+
+def isLineFull(line):
+    global mem, assoc
+    for p in range(assoc):
+        if mem[p][line][0] == 0:
+            return p
+    return -1
+        
+        
             
-def isFull(mem):
+def isFull():
+    global mem
     for r in range(nSets):
         for p in range(assoc):
             if mem[p][r][0] == 0:
